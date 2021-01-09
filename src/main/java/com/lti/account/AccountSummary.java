d08825a04cccb2e5ca65679069710045da566999
Asra <mogalasra@gmail.com>
2021-01-09 18:10:32 +0530

    2nd commit

--- src/main/java/com/lti/account/AccountSummary.java
+++ src/main/java/com/lti/account/AccountSummary.java
@@ -4,5 +4,6 @@ public class AccountSummary {
 
 	public static void main(String[] args) {
 		System.out.println("I am in AccountSummary class..");
+		System.out.println(" my  AccountSummary class..");
 	}
 }
