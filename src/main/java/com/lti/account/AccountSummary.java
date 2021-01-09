e76a3195af01e6e94ca1999db9f3b696997a1374
Asra <mogalasra@gmail.com>
2021-01-09 18:15:58 +0530

    learning

--- src/main/java/com/lti/account/AccountSummary.java
+++ src/main/java/com/lti/account/AccountSummary.java
@@ -5,5 +5,6 @@ public class AccountSummary {
 	public static void main(String[] args) {
 		System.out.println("I am in AccountSummary class..");
 		System.out.println(" my  AccountSummary class..");
+		System.out.println("   AccountSummary class..");
 	}
 }
